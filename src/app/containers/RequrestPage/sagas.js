1a03ac6bc1eba1756e8500e5350df24a8d50fb79
shaojia <jia.shao@eastiming.com>
2017-11-30 11:18:32 +0800

    fix saga for

--- src/app/containers/RequrestPage/sagas.js
+++ src/app/containers/RequrestPage/sagas.js
@@ -1,9 +1,12 @@
 import {
+  take,
+  cancel,
   fork,
   call,
   put,
   takeLatest,
 } from 'redux-saga/effects';
+import { LOCATION_CHANGE } from 'react-router-redux';
 import * as con from './constants';
 import {
   fetchListSuc,
@@ -15,12 +18,36 @@ export function* fetchListSaga() {
   yield takeLatest(con.FTECH_LIST, function* ({ payload }) {
     try {
       const result = yield call(api.fetchNetList, payload);
-      yield put(fetchListSuc(result.newslist));
+      const list = result.newslist;
+      yield put(fetchListSuc(list));
+      // yield [call(roolMap, list)];
+
+      const tasks = [];
+      for (let index = 0; index < list.length; index += 1) {
+        const element = list[index];
+        const t = yield fork(rool, element);
+        tasks.push(t);
+      }
+      yield take(LOCATION_CHANGE);
+      yield tasks.map((v) => cancel(v));
+
     } catch (error) {
       yield put(fetchListError(error));
     }
   });
 }
+
+function* rool(item) {
+  console.log(item.title);
+  if (item.title === '') yield put(fetchListError(''));
+}
+
+// function* roolMap(list) {
+//   yield put(fetchListSuc(list));
+//   list.map((item) => {
+//     return console.log(item);
+//   });
+// }
 
 export function* defaultSaga() {
   yield fork(fetchListSaga);
